2111b299727340f502b4090383ef5ed772b336df
FuenSansano <fuensanosano@gmail.com>
2022-10-14 18:53:35 +0200

    REFACTOR: create

--- app/views/admin/products/create.php
+++ app/views/admin/products/create.php
@@ -36,13 +36,13 @@
                     <label for="publisher">Editorial:</label>
                     <input type="text" name="publisher" class="form-control"
                            placeholder="Escribe la editorial del libro"
-                           value="<?= (isset($data['data']['publisher'])) ? $data['data']['publisher'] : '' ?>">
+                           value="<?= (isset($data['data']['publisher'])) ?? '' ?>">
                 </div>
                 <div class="form-group text-left">
                     <label for="pages">Páginas:</label>
                     <input type="text" name="pages" class="form-control"
                            placeholder="Escribe el número de páginas del libro"
-                           value="<?= (isset($data['data']['pages'])) ? $data['data']['pages'] : '' ?>">
+                           value="<?= (isset($data['data']['pages'])) ?? '' ?>">
                 </div>
             </div>
             <div id="course">
@@ -50,38 +50,38 @@
                     <label for="people">Público objetivo:</label>
                     <input type="text" name="people" class="form-control"
                            placeholder="Escribe el público objetivo del curso"
-                           value="<?= (isset($data['data']['people'])) ? $data['data']['people'] : '' ?>">
+                           value="<?= (isset($data['data']['people'])) ?? '' ?>">
                 </div>
                 <div class="form-group text-left">
                     <label for="objetives">Objetivos:</label>
                     <input type="text" name="objetives" class="form-control"
                            placeholder="Escribe los objetivos del curso"
-                           value="<?= (isset($data['data']['objetives'])) ? $data['data']['objetives'] : '' ?>">
+                           value="<?= (isset($data['data']['objetives'])) ?? '' ?>">
                 </div>
                 <div class="form-group text-left">
                     <label for="necesites">Conocimientos necesarios previos:</label>
                     <input type="text" name="necesites" class="form-control"
                            placeholder="Escribe los conocimientos necesarios previos"
-                           value="<?= (isset($data['data']['necesites'])) ? $data['data']['necesites'] : '' ?>">
+                           value="<?= (isset($data['data']['necesites'])) ?? '' ?>">
                 </div>
             </div>
             <div class="form-group text-left">
                 <label for="price">Precio del producto:</label>
                 <input type="text" name="price" class="form-control" pattern="^(\d|-)?(\d|,)*\.?\d*$"
                        placeholder="Escribe el precio del producto sin comas ni símbolos" required
-                       value="<?= (isset($data['data']['price'])) ? $data['data']['price'] : '' ?>">
+                       value="<?= (isset($data['data']['price'])) ?? '' ?>">
             </div>
             <div class="form-group text-left">
                 <label for="discount">Descuento del producto:</label>
                 <input type="text" name="discount" class="form-control" pattern="^(\d|-)?(\d|,)*\.?\d*$"
                        placeholder="Escribe el descuento del producto sin comas ni símbolos."
-                       value="<?= (isset($data['data']['discount'])) ? $data['data']['discount'] : '' ?>">
+                       value="<?= (isset($data['data']['discount'])) ?? '' ?>">
             </div>
             <div class="form-group text-left">
                 <label for="envio">Coste del envío del producto:</label>
                 <input type="text" name="envio" class="form-control" pattern="^(\d|-)?(\d|,)*\.?\d*$"
                        placeholder="Escribe el costo del envio del producto sin comas ni símbolos."
-                       value="<?= (isset($data['data']['send'])) ? $data['data']['send'] : '' ?>">
+                       value="<?= (isset($data['data']['send'])) ?? '' ?>">
             </div>
             <div class="form-group text-left">
                 <label for="image">Imagen del producto:</label>
@@ -91,7 +91,7 @@
                 <label for="published">Fecha del producto:</label>
                 <input type="date" name="published" class="form-control"
                        placeholder="Fecha de creación o publicación del producto (aaaa-mm-dd)"
-                       value="<?= (isset($data['data']['published'])) ? $data['data']['published'] : '' ?>">
+                       value="<?= (isset($data['data']['published'])) ??  '' ?>">
             </div>
             <div class="form-group text-left">
                 <label for="relation1">Producto relacionado:</label>
